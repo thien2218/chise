4229f55070c523dd4854c2248a8dc1677a160232
Thien Huynh <thien1367526@gmail.com>
2023-01-25 23:45:25 +0700

    Reconfigure build step

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   reactStrictMode: true,
-   images: {
-      domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
-   }
-}
+	reactStrictMode: true,
+	images: {
+		domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com"],
+	},
+   distDir: 'out',
+	experimental: {
+		images: {
+			unoptimized: true,
+		},
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/design.js
+++
@@ -1,7 +0,0 @@
-const Design = () => {
-	return (
-		<></>
-	);
-};
-
-export default Design;
